5dd917fb296b596df20ceebaa9b47e1b2081750f
Nickita Danilovich <nickitadanilovich@gmail.com>
2022-12-31 16:43:21 -0500

    commit tree 4aedbc1f0bb95bfec0f5b7f53c3c6661541a3467

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+function writeCards(names, event ){
+  let newArray = [];
+  for(let num = 0; num <= names.length - 1; num++){
+  newArray.push(`Thank you, ${names[num]}, for the wonderful ${event} gift!`);
+    console.log(newArray);
+  }
+  return newArray;
+}
+
+wrapGifts(gifts);
+
+function countDown(count){
+  while (count >= 0){
+    console.log(count);
+    count--;
+  }
+}
