d1d326fd61236c30d51c64403622e9d7ee727c40
Nickita Danilovich <nickitadanilovich@gmail.com>
2023-01-12 10:59:12 -0500

    passed loops

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function countDown(){
+    for (let i = 10; i >= 0; i--){
+        console.log(i);
+    }
+}
+
+function writeCards(names, celebrations){
+    let card =[];
+    for (let j = 0; j < names.length; j++){
+       let message = `Thank you, ${names[j]}, for the wonderful ${celebrations} gift!`;
+        card.push(message);
+    }
+     return card;
+}
